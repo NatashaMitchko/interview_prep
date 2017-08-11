1d20263a2577f95d20c5e2c929f198166ae4372b
Natasha Mitchko <natasha@natasha.mitchko.com>
2017-08-11 13:55:35 -0400

    Route Between Nodes Resursive Solution
    Depth first search written as a method on the graph class. Utilizes
    a set of seen nodes and iterates through the difference between
    the adjacency set and the seen set resursively.

--- route_between_nodes.py
+++ route_between_nodes.py
@@ -28,13 +28,61 @@ def route_between_nodes(node1, node2, directed_graph):
 		for i in current.list:
 			route_between_nodes(i, node2, directed_graph)
 
-
-
 """
 ################################################################################
 Solution inspired by looking at the solution(s) of others
 ################################################################################
 """
+# This solution is one where the method is defined on the Graph class itself
+class Node(object):
+	"""Node for Graph"""
+
+	def __init__(self, name, adjacent=None):
+		"""Create a node with a name and optional adjcentcy list"""
+
+		if adjacent is None:
+			adjacent = set()
+
+		assert isinstance(adjacent, set), \
+
+		self.name = name
+		self.adjacent = adjacent
+
+	def __repr__(self):
+		return "<{} Node>".format(self.name)
+
+class Graph(object):
+	"""Directed graph with is_reachable method"""
+
+	def __init__(self):
+		self.nodes = set()
+
+	def __repr__(self):
+		return "<{}>".format([n.name for n in self.nodes])
+
+	def add(self, item):
+		self.nodes.add(item)
+
+	def add_relation(self, item1, item2):
+		"""adds relation in one direction"""
+		item1.adjacent.add(item2)
+
+	def is_reachable(self, node1, node2, seen=None):
+		"""Checks if you can get from node1 to node2
+		This is depth first.
+		"""
+		if not seen:
+			seen = set()
+
+		if node1 is node2:
+			return True
+
+		seen.add(node1)
+
+		for node in node1.adjacent - seen:
+			if self.is_reachable(node, node2, seen):
+				return True
+		return False
 
 if __name__ == "__main__":
     import doctest
