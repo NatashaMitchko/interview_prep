d071217289c6a9aae6cd03bde7743940f4722d88
Natasha Mitchko <Natasha@Natashas-MacBook-Air.local>
2018-07-10 20:12:10 -0400

    getting closer

--- urlify.py
+++ urlify.py
@@ -10,8 +10,8 @@ def urlify(s):
 		if s[i] == " " and i != 0 and s[i-1] != " ":
 			e = i
 			contents.append(s[b:e])
-		elif i != 0 and i != len(s) - 1 and s[i+1] != " ":
-			b = i
+		elif i != 0 and i != len(s) - 1 and s[i+1] != " " and s[i-1] == " ":
+			b = i + 1
 		elif i == len(s) - 1 and s[i] != " ":
 			contents.append(s[b:])
 	return "%20".join(contents)
