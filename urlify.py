b34c2e098eedf13a2f112320101cb5b5edbb3ff2
Natasha Mitchko <Natasha@Natashas-MacBook-Air.local>
2018-07-11 19:40:18 -0400

    working

--- urlify.py
+++ urlify.py
@@ -1,24 +1,32 @@
+import pytest
+
 def urlify(s):
 	"""
 	>>> urlify("a  b  cd  ")
 	a%20b%20cd
 	"""
 	contents = []
+	ls = len(s)
 	b = 0
 	e = 0
-	for i in range(len(s)):
-		if s[i] == " " and i != 0 and s[i-1] != " ":
+	for i in range(ls):
+		if i == 0 and s[i] != " ":
+			b = 0
+		if i == 0 and s[i] == " ":
+			b = i + 1
+		if i != 0 and s[i] == " " and s[i-1] != " ":
 			e = i
 			contents.append(s[b:e])
-		elif i != 0 and i != len(s) - 1 and s[i+1] != " " and s[i-1] == " ":
-			b = i + 1
-		elif i == len(s) - 1 and s[i] != " ":
+		if i != 0 and s[i] != " " and s[i-1] == " ":
+			b = i
+		if i == ls - 1 and s[i] != " ":
 			contents.append(s[b:])
 	return "%20".join(contents)
 
-
-print urlify("a  b  c")
-print urlify("   b   ")
-print urlify(" abc")
-print urlify(" ab c ")
-print urlify("abc    d")
+@pytest.mark.parametrize("s, expected", [("a  b  cd ", "a%20b%20cd"),
+							             ("a  b  c", "a%20b%20c"),
+							             (" abc", "abc"),
+							             (" ab c ", "ab%20c"),
+							             ("abc   d", "ab%20d")])
+def test_urlify(s, expected):
+	assert urlify(s) == expected
