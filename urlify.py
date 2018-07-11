2ef649c6b0b230473df327ac1ac4a65a0317e484
Natasha Mitchko <Natasha@Natashas-MacBook-Air.local>
2018-07-10 20:06:12 -0400

    work in progress

---
+++ urlify.py
@@ -0,0 +1,24 @@
+def urlify(s):
+	"""
+	>>> urlify("a  b  cd  ")
+	a%20b%20cd
+	"""
+	contents = []
+	b = 0
+	e = 0
+	for i in range(len(s)):
+		if s[i] == " " and i != 0 and s[i-1] != " ":
+			e = i
+			contents.append(s[b:e])
+		elif i != 0 and i != len(s) - 1 and s[i+1] != " ":
+			b = i
+		elif i == len(s) - 1 and s[i] != " ":
+			contents.append(s[b:])
+	return "%20".join(contents)
+
+
+print urlify("a  b  c")
+print urlify("   b   ")
+print urlify(" abc")
+print urlify(" ab c ")
+print urlify("abc    d")
